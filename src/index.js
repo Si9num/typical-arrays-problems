998098122f240feb437a51223b293910b1a9bdbe
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-05 00:15:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length == 0) {
+        return 0;
+
+    }
+    else if (array.length == 0) {
+        return 0;
+    }
+  return Math.min.apply(null,array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0) {
+        return 0;
+
+    }
+    else if (array.length == 0) {
+        return 0;
+    }
+  return Math.max.apply(null,array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0) {
+        return 0;
+
+    }
+    else if (array.length == 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let a of array) {
+        sum += a;
+    
+    }
+  return sum/array.length;
 }
